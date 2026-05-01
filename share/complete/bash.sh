eb7445ba8578dd1e508437a6c6bde85f4e4dd5fc
Ingy döt Net <ingy@ingy.net>
2026-05-01 11:59:00 -0400

    Support --stash, --link and --unlink

--- share/complete/bash.sh
+++ share/complete/bash.sh
@@ -1,12 +1,21 @@
 # shellcheck shell=bash
 
 _pkio() {
+  # Auto-reload this file when it changes
+  local _file=$PKIO_ROOT/share/complete/bash.sh
+  if [[ $_file -nt ${_pkio_mtime_ref:-/} ]]; then
+    _pkio_mtime_ref=$(mktemp)
+    source "$_file"
+    _pkio "$@"
+    return
+  fi
+
   local cur prev cmds flags
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-  flags="--config --show-config --shell --complete --list --update --reset --help"
+  flags="--config --show-config --stash= --link --unlink --shell --complete --list --update --reset --help"
 
   if [[ $COMP_CWORD -eq 1 ]]; then
     if [[ $cur == --config=* ]]; then
--- share/complete/fish.fish
+++ share/complete/fish.fish
@@ -4,6 +4,9 @@ complete -c pkio -l config -d 'Edit config' -x
 complete -c pkio -l show-config -d 'Print merged config for a program'
 complete -c pkio -l shell -d 'Start sandboxed subshell' -x -a 'bash zsh fish'
 complete -c pkio -l complete -d 'Output completion code' -x -a 'bash zsh fish'
+complete -c pkio -l stash -d 'Move file to config stash and symlink' -x -a '(__fish_complete_path)'
+complete -c pkio -l link -d 'Create symlinks for stashed files' -x
+complete -c pkio -l unlink -d 'Remove symlinks to stashed files'
 complete -c pkio -l list -d 'List programs with curated defaults'
 complete -c pkio -l update -d 'Update pkio'
 complete -c pkio -l reset -d 'Remove cached dependencies'
