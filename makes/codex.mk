29a4a7a66ebc1be91761f936e6a5124d09fbe513
Ingy döt Net <ingy@ingy.net>
2026-06-02 22:42:41 -0400

    Add node for codex

--- makes/codex.mk
+++ makes/codex.mk
@@ -3,10 +3,12 @@ PKIO-CODEX-LOADED := true
 PKIO-PROGRAM-LOADED := true
 
 include $(MAKES)/rg.mk
+include $(MAKES)/node.mk
 
 PKIO-NONO-OPTS += --read $(LOCAL-BIN)
+PKIO-NONO-OPTS += --read $(NODE-BIN)
 PKIO-EXPORT-ENV += PATH=$(PATH)
 
-pkio-setup: $(NONO) $(RG)
+pkio-setup: $(NONO) $(RG) $(NODE)
 
 endif
